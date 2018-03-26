dc4ce19eedb2854bc09acf9aa379d1ec0e03fba2
krk chaitanya <krkchaitanya42@gmail.com>
2018-03-26 11:01:29 -0500

    appRefactoring

--- gulpfile.js
+++ gulpfile.js
@@ -20,11 +20,11 @@ gulp.task('js', function(){
 // Watch Sass & Server
 gulp.task('serve', ['sass'], function(){
   browserSync.init({
-    server: "./src"
+    server: "./"
   });
 
   gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
-  gulp.watch("src/*.html").on('change', browserSync.reload);
+  gulp.watch("index.html").on('change', browserSync.reload);
 });
 
 // Move Fonts Folder to src/fonts
